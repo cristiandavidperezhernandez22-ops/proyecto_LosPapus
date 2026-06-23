e786fe838e9b76ddf48e71270b1b38ec1bcd5c3e
Ivan ND <ivanhumberto3007@gmail.com>
2026-06-23 18:13:18 -0500

    Class vehicle

---
+++ model/Vehicle.py
@@ -0,0 +1,20 @@
+class Vehicle:
+    def __init__(self, tire_number, charge_capacity, fuel, capacity, brand, on):
+        self.tire_number = tire_number
+        self.charge_capacity = charge_capacity
+        self.fuel = fuel
+        self.capacity = capacity
+        self.brand = brand
+        self.on = on
+    
+    def start(self):
+        pass
+
+    def drive(self):
+        pass
+
+    def brake(self):
+        pass
+
+    def honk(self):
+        pass
