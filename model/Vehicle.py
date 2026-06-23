48f15af074f317baca91c42a416b0d14f9d8b5f1
Cristian Perez Hernandez <cristiandavidperezhernandez22@gmail.com>
2026-06-23 18:20:00 -0500

    Actualizar constructor de la clase Vehicle para corregir parámetros y tipos

--- model/Vehicle.py
+++ model/Vehicle.py
@@ -1,9 +1,16 @@
 class Vehicle:
-    def __init__(self, tire_number, charge_capacity, fuel, capacity, brand, on):
+    def __init__(self,
+                 tire_number:int,
+                 charge_capacity:str,
+                 fuel:str,
+                 passengers:int,
+                 brand:str,
+                 on:bool
+                 ):
         self.tire_number = tire_number
         self.charge_capacity = charge_capacity
         self.fuel = fuel
-        self.capacity = capacity
+        self.passengers = passengers
         self.brand = brand
         self.on = on
     
